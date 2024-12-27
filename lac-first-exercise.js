1ec318d09dc6576ccdd60886e09b78d2167b028f
madeelahsaan <155633238+madeelahsaan@users.noreply.github.com>
2024-12-27 16:58:53 +0500

    Day first of the 30 days journey of JavaScript

---
+++ lac-first-exercise.js
@@ -0,0 +1,24 @@
+// VARIABLE & DATA TYPE
+
+// problem profile details
+
+const profile_info = {
+    userName : "Unknown",
+    isFollow : true,
+    post : 195,
+    followers : 569,
+    following : 4,
+    bio : "I'm bignner js student"
+};
+console.log(profile_info);
+
+
+// product details
+const product_info = {
+    tittle : "Parker jotter (Black)",
+    rating : 4,
+    is_Deal : true ,
+    price : 275 ,
+    sale : 5
+}
+console.log(product_info);
