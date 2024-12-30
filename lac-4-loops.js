ebf2398375fc473a1a862c7e2b01f51e843aaf46
madeelahsaan <155633238+madeelahsaan@users.noreply.github.com>
2024-12-30 23:56:26 +0500

    day4 of 30day learning javascript journey

---
+++ lac-4-loops.js
@@ -0,0 +1,52 @@
+//print 5 times any text
+
+// for (let i=0; i<5; i++){
+//     console.log("Hi I'm Unknown");
+// }
+
+
+
+//calculate sum of 1 to 5
+
+// let sum = 0;
+// for(let i=1; i<=5; i++){
+//     sum = sum+i;
+// }
+// console.log("Sum of 1 to 5 is : ",sum);
+
+
+
+//print 1to5 using while and while work only those time when condition is true
+
+// let i = 1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 20;
+// while (i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+
+//Do while
+// let i = 20;
+// do{
+//     console.log(i);
+// }while(i<=5);
+
+
+
+//PRACTICE QUESTION
+
+//NO:1 PRINT all even number 0 to 100
+/*
+for(let i=0; i<=100; i++){
+    if(i%2===0){
+        console.log(i);
+    }
+}
+    */
+
