239f9b1f43b5217c4141bb85faa29512a051fbbc
madeelahsaan <155633238+madeelahsaan@users.noreply.github.com>
2025-01-02 16:41:51 +0500

    Solve the Praction of array

--- lac-6-arrays.js
+++ lac-6-arrays.js
@@ -8,6 +8,7 @@ console.log(student);
 */
 
 
+/*
 let array = ["Ali", "Azeem", "Faraz", "Sarmad", "hamza"];
 console.log("length of array is : ",array.length);
 
@@ -15,11 +16,14 @@ console.log(`the value/element at index 3 : ${array[3]}`);
 
 array[4] = "Noman"
 console.log(array);
+*/
 
 // way to change the case of element in Arrays.
+/*
 for(let student of array){
     console.log(student.toUpperCase());
 }
+*/
 
 
 // PRACTICE QUESTION NO: 1
@@ -32,4 +36,33 @@ for(let num of marks){
 }
 let avg = sum/(marks.length);
 console.log(avg);
+*/
+
+// let num = 300;
+// let offer = (num/100)*90;
+// console.log(offer);
+
+//PRACTICE QUESTION NO: 2
+//FOR A GIVEN ARRAY with the price of item -> [250, 645, 300, 900, 50]. All item have an offer of 10% off on them. Change the array to store the final price after applying offer.
+
+//using for loop
+/*
+let price = [250, 645, 300, 900, 50];
+for(let i=0; i<price.length; i++){
+    let offer = price[i]/10;
+    price[i] -= offer;
+}
+console.log(price);
+*/
+
+//Using for-of
+/*
+let price = [250, 645, 300, 900, 50];
+let i =0;
+for (let val of price){
+    let offer = val/10;
+    price[i] -= offer;
+    i++;
+}
+console.log(`List of price after offer : ${price}`);
 */
