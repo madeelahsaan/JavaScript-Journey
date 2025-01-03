26a1616af26e8ecd55529fff9328633db4299c53
madeelahsaan <155633238+madeelahsaan@users.noreply.github.com>
2025-01-03 18:31:30 +0500

    practice of Array

---
+++ lac-7-prac.js
@@ -0,0 +1,47 @@
+//PRATICE QUESTION NO: 1
+//Create any array to store companies name -> "Bloomberg", "Microsfot", "Uber", "Google", "IBM", "Netflix"
+//1. Remove the first Company name.
+//2. Remove Uber and add ola in it's place.
+//3. Add Amazon at the end.
+
+let companies = ["Bloomberg", "Microsfot", "Uber", "Google", "IBM", "Netflix"];
+
+
+//1. Remove the first Company name.
+//console.log(companies);
+// let delValue = companies.shift();
+// console.log(delValue)
+
+
+//2. Remove Uber and add ola in it's place.
+// console.log(companies);
+// companies.splice(2, 1, "ola");
+
+
+//3. Add Amazon at the end.
+// console.log(companies);
+// companies.push("Amazon");
+
+
+console.log("after changing",companies);
+
+//PRACTICE QUESTION NUMBER 2
+
+//Create a array of numbers and take input from the user to add in array;
+// let numbers = [1, 4, 6, 7, 9, 11];
+// console.log(numbers)
+// let userNum = prompt("Enter a number that you wnat to add in array");
+// numbers.push(Number(userNum));
+// console.log(numbers);
+
+
+//Keep adding numbers into array until 0 is added to array
+// let numbers = [1, 4, 6, 7, 9, 11];
+// let userNum;
+// do{
+//     userNum = prompt("Enter a number that you wnat to add in array");
+//     numbers.push(Number(userNum));
+// } while(userNum != 0);
+// console.log(numbers);
+
+
