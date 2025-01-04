ae4305d13c05ba5e1a3580d310ad75d016171067
madeelahsaan <155633238+madeelahsaan@users.noreply.github.com>
2025-01-04 23:48:28 +0500

    day8 of 30days journey, today i learn filter, reduce, map methods and forEach loop

--- lac-8-function.js
+++ lac-8-function.js
@@ -23,14 +23,56 @@
 
 // console.log(printHello());//call func
 
-function numOfVowel(str) {
-    let num=0 ;
-    for (let i of str) {
-        
-        if (i === "a" || i === "e" || i === "i" || i === "o" || i === "u"){
-            num++;
-            console.log(num)
-        }
-    }
+// function numOfVowel(str) {
+//     let num=0 ;
+//     str = str.toLowerCase();
+//     for (let i of str) {
+//         if (i === "a" || i === "e" || i === "i" || i === "o" || i === "u"){
+//             num++;
+//         }
+//     }
+//     console.log(num)
+// }
+// console.log(numOfVowel("Adeel"));
+
+// function vowelCount(str){
+//     let vowel = ["a","e","i","o","u"];
+//     let count = 0;
+//     for(let char of str){
+//         for(let ele of vowel){
+//             if(ele === char.toLowerCase()){
+//                 count++;
+//             }
+//         }
+//     }
+//     console.log(count)
+// }
+// console.log(vowelCount("Adeel"));
+
+// const vowelCount = (str)=>{
+//     let count=0;
+//     for(let char of str){
+//         if( char==="a" || char==="e" || char==="i" || char==="o" || char==="u"){
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+// console.log(vowelCount("adeel"))
+
+let marks = {
+    adeel :45,
+    arbaz :0o5,
+    azaeem :55,
+    ali :15
 }
-console.log(numOfVowel("adeel"));
+for (let i=0; i<Object.keys(marks).length;i++){
+    // console.log(marks[Object.keys(marks)[i]]);// it's for value of keys
+    console.log(Object.keys(marks)[i]);//it's for keys
+}
+
+//for-in
+for(let key in marks){
+    console.log(key);//its for keys
+    console.log(marks[key]);//it's for value of keys
+}
---
+++ lac-9-foreachloop.js
@@ -0,0 +1,10 @@
+// for a given array of number print the square of each value using the foreachloop
+let num = [1, 2, 3, 4];
+// num.forEach((val) => {
+//     console.log(val**2);
+// });
+
+// let calSquare = (val) => {
+//     console.log(val ** 2);
+// }
+// num.forEach(calSquare);
